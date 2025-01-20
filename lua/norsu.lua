ab95598c9e7cc6a595fd8ec0251f19d3aebc8d4b
Sergey Lavrent <91432388+hiimsergey@users.noreply.github.com>
2025-01-20 22:42:25 +0100

    Wrote Autocommand

--- lua/norsu.lua
+++ lua/norsu.lua
@@ -1,12 +1,11 @@
 local M = {}
 
-local pickers = require "telescope.pickers"
-local finders = require "telescope.finders"
-local conf = require("telescope.config").values
-local actions = require "telescope.actions"
+local function list_wikis_telescope()
+    local actions = require "telescope.actions"
+    local conf = require("telescope.config").values
+    local finders = require "telescope.finders"
+    local pickers = require "telescope.pickers"
 
--- TODO the preview window shows the last opened note
-vim.api.nvim_create_user_command("NorsuListWikis", function()
     pickers.new({}, {
         prompt_title = "Norsu Wikis",
         finder = finders.new_table {
@@ -22,6 +21,44 @@ vim.api.nvim_create_user_command("NorsuListWikis", function()
             return true
         end
     }):find()
-end, {})
+end
+
+M.config = {
+    wikis = {},
+    list_wikis_func = list_wikis_telescope
+}
+
+function M.setup(config)
+    M.config = vim.tbl_deep_extend("force", M.config, config or {})
+end
+
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+    pattern = "*.md",
+    callback = function(_)
+        print(vim.fn.expand("%:p:h"))
+    end
+})
+
+-- TODO the preview window shows the last opened note
+vim.api.nvim_create_user_command("NorsuListWikis", M.config.list_wikis_func, {})
+
+-- TODO only make it availble if the pwd is the wiki
+vim.api.nvim_create_user_command("NorsuNewFolder", function(opts)
+    if opts.args == "" then
+        opts.args = vim.fn.input("Enter new folder name: ")
+    end
+    -- TODO PLAN
+    -- create folder
+end, { nargs = "?" })
+
+-- TODO only make it availble if the pwd is the wiki
+vim.api.nvim_create_user_command("NorsuNewNote", function(opts)
+    if opts.args == "" then
+        opts.args = vim.fn.input("Enter new note name: ")
+    end
+    -- TODO PLAN
+    -- vim.cmd.edit(WIKI_PATH .. opts.args)
+    -- reindex
+end, { nargs = "?" })
 
 return M
