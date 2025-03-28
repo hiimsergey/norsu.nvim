46e8e7125ac389b90efb50a700f815f138be55c1
Sergey Lavrent <91432388+hiimsergey@users.noreply.github.com>
2025-03-28 20:05:44 +0100

    Structured.

--- lua/norsu.lua
+++ lua/norsu.lua
@@ -1,6 +1,18 @@
+-- TODO NOW
+-- write commands:
+--     list notes in this wiki (telescope)
+--     list norsu commands (telescope)
+--     view backlinks (telescope)
+-- file cache:
+--     where links are
+--     backlinks
+-- autocomplete:
+--     note links
+-- table autocomplete
+local vim = vim
 local M = {}
 
-local function list_wikis_telescope()
+local function list_wikis_telescope(config)
     local actions = require "telescope.actions"
     local conf = require("telescope.config").values
     local finders = require "telescope.finders"
@@ -10,7 +22,7 @@ local function list_wikis_telescope()
         prompt_title = "Norsu Wikis",
         finder = finders.new_table {
             -- TODO PLACEHOLDER
-            results = { "one", "two", "three" }
+            results = config.wikis
         },
         sorter = conf.generic_sorter(),
         attach_mappings = function(prompt_bufnr)
@@ -23,42 +35,59 @@ local function list_wikis_telescope()
     }):find()
 end
 
-M.config = {
-    wikis = {},
-    list_wikis_func = list_wikis_telescope
-}
+local function create_exclusive_commands()
+    -- TODO only make it availble if the pwd is the wiki
+    vim.api.nvim_create_user_command("NorsuNewFolder", function(opts)
+        if opts.args == "" then
+            opts.args = vim.fn.input("Enter new folder name: ")
+        end
+        -- TODO PLAN
+        -- create folder
+        print("about to mkdir " .. opts.args)
+    end, { nargs = "?" })
 
-function M.setup(config)
-    M.config = vim.tbl_deep_extend("force", M.config, config or {})
+    -- TODO only make it availble if the pwd is the wiki
+    vim.api.nvim_create_user_command("NorsuNewNote", function(opts)
+        if opts.args == "" then
+            opts.args = vim.fn.input("Enter new note name: ")
+        end
+        -- TODO PLAN
+        -- vim.cmd.edit(WIKI_PATH .. opts.args)
+        -- reindex
+        print("about to make note" .. opts.args)
+    end, { nargs = "?" })
 end
 
-vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
-    pattern = "*.md",
-    callback = function(_)
-        print(vim.fn.expand("%:p:h"))
-    end
-})
+local function create_ubiquitous_commands()
+    -- TODO the preview window shows the last opened note
+    vim.api.nvim_create_user_command("NorsuListWikis", function()
+        M.config.list_wikis_cb(M.config)
+    end, {})
 
--- TODO the preview window shows the last opened note
-vim.api.nvim_create_user_command("NorsuListWikis", M.config.list_wikis_func, {})
+    -- TODO CONSIDER NorsuOpenLast
+end
 
--- TODO only make it availble if the pwd is the wiki
-vim.api.nvim_create_user_command("NorsuNewFolder", function(opts)
-    if opts.args == "" then
-        opts.args = vim.fn.input("Enter new folder name: ")
-    end
-    -- TODO PLAN
-    -- create folder
-end, { nargs = "?" })
+M.config = {
+    wikis = {},
+    list_wikis_cb = list_wikis_telescope
+}
 
--- TODO only make it availble if the pwd is the wiki
-vim.api.nvim_create_user_command("NorsuNewNote", function(opts)
-    if opts.args == "" then
-        opts.args = vim.fn.input("Enter new note name: ")
-    end
-    -- TODO PLAN
-    -- vim.cmd.edit(WIKI_PATH .. opts.args)
-    -- reindex
-end, { nargs = "?" })
+M.setup = function(config)
+    M.config = vim.tbl_deep_extend("force", M.config, config or {})
+
+    vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+        pattern = "*.md",
+        callback = function()
+            local cur_note_dir = vim.fn.expand "%:p:h"
+            for _, wiki in pairs(config.wikis) do
+                if vim.fn.expand(wiki.path) == cur_note_dir then
+                    create_exclusive_commands()
+                    break
+                end
+            end
+            create_ubiquitous_commands()
+        end
+    })
+end
 
 return M
