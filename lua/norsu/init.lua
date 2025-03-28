56f95417ffd0113ba4e5988bd1dcfe8d8c238954
Sergey Lavrent <91432388+hiimsergey@users.noreply.github.com>
2025-03-28 20:25:01 +0100

    Structured 2: Electric Submodules

--- lua/norsu/init.lua
+++ lua/norsu/init.lua
@@ -1,4 +1,10 @@
 -- TODO NOW
+-- markdown features:
+--     basic link functionality:
+--         hide brackets when leaving line
+--         <enter> goes there
+--     link autocomplete (with #headings)
+--     table autocomplete
 -- write commands:
 --     list notes in this wiki (telescope)
 --     list norsu commands (telescope)
@@ -6,34 +12,10 @@
 -- file cache:
 --     where links are
 --     backlinks
--- autocomplete:
---     note links
--- table autocomplete
 local vim = vim
+local norsu_telescope = require "norsu.telescope"
+
 local M = {}
-
-local function list_wikis_telescope(config)
-    local actions = require "telescope.actions"
-    local conf = require("telescope.config").values
-    local finders = require "telescope.finders"
-    local pickers = require "telescope.pickers"
-
-    pickers.new({}, {
-        prompt_title = "Norsu Wikis",
-        finder = finders.new_table {
-            -- TODO PLACEHOLDER
-            results = config.wikis
-        },
-        sorter = conf.generic_sorter(),
-        attach_mappings = function(prompt_bufnr)
-            actions.select_default:replace(function()
-                actions.close(prompt_bufnr)
-                -- TODO make it do something
-            end)
-            return true
-        end
-    }):find()
-end
 
 local function create_exclusive_commands()
     -- TODO only make it availble if the pwd is the wiki
@@ -69,7 +51,7 @@ end
 
 M.config = {
     wikis = {},
-    list_wikis_cb = list_wikis_telescope
+    list_wikis_cb = norsu_telescope.list_wikis
 }
 
 M.setup = function(config)
@@ -85,9 +67,10 @@ M.setup = function(config)
                     break
                 end
             end
-            create_ubiquitous_commands()
         end
     })
+
+    create_ubiquitous_commands()
 end
 
 return M
