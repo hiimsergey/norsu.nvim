13f58f6fd777b3dcabc262ec146cacb903994ce1
Sergey Lavrent <91432388+hiimsergey@users.noreply.github.com>
2025-05-22 23:54:13 +0200

    Proper(er) module setup

--- lua/norsu/commands.lua
+++ lua/norsu/commands.lua
@@ -1,95 +1,100 @@
 local vim = vim
+
 local actions = require "telescope.actions"
 local conf = require "telescope.config".values
 local finders = require "telescope.finders"
 local pickers = require "telescope.pickers"
 
-local ctx = require "norsu.context"
-
 local M = {}
 
--- TODO NOW
--- where to store ctx variables (current wiki)
--- write the picker
--- vim.cmd.edit command
--- log msg
-M.NorsuNewNote = function(opts)
-    if opts.args == "" then
-        -- TODO use picker
-        print "TODO no args :("
-        return
-    end
+M.register_exclusive = function()
+    -- TODO NOW
+    -- write the picker
+    -- vim.cmd.edit command
+    -- log msg
+    vim.api.nvim_buf_create_user_command(0, "NorsuOpen", function(opts)
+        if opts.args == "" then
+            -- TODO use picker
+            print "TODO no args :("
+            return
+        end
 
-    if opts.args:sub(-3, -1) ~= ".no" then
-        opts.args = opts.args .. ".no"
-        -- TODO NOW
-    end
+        if opts.args:sub(-3, -1) ~= ".no" then
+            opts.args = opts.args .. ".no"
+        end
 
-    vim.cmd.edit(ctx.cur_wiki .. opts.args)
-end
-
-M.NorsuNewFolder = function(opts)
-    if opts.args == "" then
-        -- TODO use picker
-        print "TODO no args :("
-        return
-    end
-
-    vim.fn.mkdir(ctx.cur_wiki .. opts.args)
-    vim.notify("Norsu: Made folder " .. opts.args, vim.log.levels.INFO)
-end
-
--- TODO
-M.NorsuMove = function(opts)
-    if opts.args == "" then
-        -- TODO use picker
-        print "TODO no args :("
-        return
-    end
-
-    opts.args = vim.split(opts.args, " ", { trimempty = true })
-
-    local dest = opts.args[#opts.args]
-    if not vim.uv.fs_stat(dest) then
-        print(dest .. " doesnt exist :( TODO")
-        return
-    end
-
-    -- TODO PLAN
-    -- if there is only arg the src file is the currently open one
-    for i = 1, #opts.args - 1 do
         -- TODO PLAN
-        -- move opts.args[i] to dest
-        if not vim.fn.isabsolute(opts.args[i]) then
-            opts.args[i] = ctx.cur_wiki .. opts.args[i]
+        -- if bang, touch the file
+        -- else throw error if non-existant; return
+        -- finally edit the file
+        --vim.cmd.edit(ctx.cur_wiki .. opts.args)
+    end, { bang = true, nargs = "?" })
+
+    vim.api.nvim_buf_create_user_command(0, "NorsuNewFolder", function(opts)
+        if opts.args == "" then
+            -- TODO use picker
+            print "TODO no args :("
+            return
         end
-        vim.uv.fs_rename(opts.args[i], dest .. opts.args[i])
-    end
+
+        vim.fn.mkdir(ctx.cur_wiki .. opts.args)
+        vim.notify("Norsu: Made folder " .. opts.args, vim.log.levels.INFO)
+    end, { nargs = "?" })
+
+    -- TODO
+    vim.api.nvim_buf_create_user_command(0, "NorsuMove", function(opts)
+        if opts.args == "" then
+            -- TODO use picker
+            print "TODO no args :("
+            return
+        end
+
+        opts.args = vim.split(opts.args, " ", { trimempty = true })
+
+        local dest = opts.args[#opts.args]
+        if not vim.uv.fs_stat(dest) then
+            print(dest .. " doesnt exist :( TODO")
+            return
+        end
+
+        -- TODO PLAN
+        -- if there is only arg the src file is the currently open one
+        for i = 1, #opts.args - 1 do
+            -- TODO PLAN
+            -- move opts.args[i] to dest
+            if not vim.fn.isabsolute(opts.args[i]) then
+                opts.args[i] = ctx.cur_wiki .. opts.args[i]
+            end
+            vim.uv.fs_rename(opts.args[i], dest .. opts.args[i])
+        end
+    end, { nargs = "*" })
 end
 
--- TODO NOW MOVE picker to abstraction
-M.NorsuOpenWiki = function(opts)
-    print "TODO listing wikis"
+M.register_ubiquitous = function()
+    vim.api.nvim_buf_create_user_command(0, "NorsuOpenWiki", function(opts)
+        -- TODO handle args
+        print "TODO listing wikis"
 
-    pickers.new({}, {
-        prompt_title = "Norsu Wikis",
-        finder = finders.new_table {
-            -- TODO PLACEHOLDER
-            results = config.wikis
-        },
-        sorter = conf.generic_sorter(),
-        attach_mappings = function(prompt_bufnr)
-            actions.select_default:replace(function()
-                actions.close(prompt_bufnr)
-                -- TODO make it do something
-            end)
-            return true
-        end
-    }):find()
+        pickers.new({}, {
+            prompt_title = "Norsu Wikis",
+            finder = finders.new_table {
+                -- TODO PLACEHOLDER
+                results = config.wikis
+            },
+            sorter = conf.generic_sorter(),
+            attach_mappings = function(prompt_bufnr)
+                actions.select_default:replace(function()
+                    actions.close(prompt_bufnr)
+                    -- TODO make it do something
+                end)
+                return true
+            end
+        }):find()
 
-    -- TODO PLAN
-    -- open the latest note of selected wiki
-    -- a simple vim.cmd.edit will suffice
+        -- TODO PLAN
+        -- open the latest note of selected wiki
+        -- a simple vim.cmd.edit will suffice
+    end, { nargs = "?" })
 end
 
 return M
--- lua/norsu/init.lua
+++ lua/norsu/init.lua
@@ -16,50 +16,58 @@
 -- help pages
 -- set filetype in lualine
 local vim = vim
+
 local cmd = require "norsu.commands"
-local ctx = require "norsu.context"
+local group = vim.api.nvim_create_augroup("Norsu", { clear = true })
 
 local M = {}
 
-local function create_exclusive_commands()
-    vim.api.nvim_create_user_command("NorsuNewNote", cmd.NorsuNewNote, { nargs = "?" })
-    vim.api.nvim_create_user_command("NorsuNewFolder", cmd.NorsuNewFolder, { nargs = "?" })
-    vim.api.nvim_create_user_command("NorsuMove", cmd.NorsuMove, { nargs = "*" })
-end
-
-local function create_ubiquitous_commands()
-    -- TODO the preview window shows the last opened note
-    vim.api.nvim_create_user_command("NorsuListWikis", cmd.NorsuOpenWiki, {})
-
-    -- TODO CONSIDER NorsuOpenLast
-end
-
 M.config = {
-    wikis = {}
+    -- Deepest directory containing all detectable wikis on the system.
+    -- The wiki indexing algorithm stops crawling here.
+    -- All wikis outside the root will not be indexed.
+    root = vim.fn.expand "~"
 }
 
 M.setup = function(config)
     M.config = vim.tbl_deep_extend("force", M.config, config or {})
     vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+        group = group,
+        pattern = "*.no",
         callback = function()
-            local cur_note_dir = vim.fn.expand "%:p:h"
-            for _, wiki in pairs(config.wikis) do
-                local wiki_path_abs = vim.fn.expand(wiki.path)
-                if cur_note_dir:sub(1, #wiki_path_abs) == wiki_path_abs then
-                    ctx.cur_wiki = wiki_path_abs .. "/"
-                    create_exclusive_commands()
-                    -- TODO CONSIDER register autocmd
-                    -- update last opened note in this wiki
-                    -- reindex on save:
-                    --    update list of notes
-                    --    update links
+            -- Abort indexing if opened file is outside wikis root
+            local file = vim.fn.expand "%:p"
+            if file:sub(1, #M.config.root) ~= M.config.root then return end
+
+            -- TODO ADD cache
+            local dir = vim.fn.expand "%:p:h"
+            while true do
+                local candidate = dir .. "/.norsu.json"
+                if vim.fn.filereadable(candidate) == 1 then
+                    vim.b.wiki_root = dir
                     break
                 end
+
+                dir = vim.fn.fnamemodify(dir, ":h")
+                if dir == M.config.root then return end
             end
+
+            cmd.register_exclusive()
+
+            vim.api.nvim_create_autocmd("BufWritePost", {
+                buffer = 0,
+                group = group,
+                callback = function()
+                    -- TODO PLAN
+                    -- reindex:
+                    -- find new links
+                    vim.cmd.quit()
+                end
+            })
         end
     })
 
-    create_ubiquitous_commands()
+    cmd.register_ubiquitous()
 end
 
 return M
